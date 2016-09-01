48b67609e612d6e978584c89821394ee8a188d95
Eduardo Fernandes <idasbistefernandes@gmail.com>
2016-09-01 11:45:39 +0000

    Add add/remove book logic

--- site/js/views/book.js
+++ site/js/views/book.js
@@ -14,6 +14,17 @@ app.BookView = Backbone.View.extend({
     
     template: _.template($("#bookTemplate").html()),
     
+    events: {
+        "click .delete": "deleteBook"
+    },
+    
+    deleteBook: function () {
+        // Delete model
+        this.model.destroy();
+        // Delete view
+        this.remove();
+    },
+    
     render: function () {
         this.$el.html(this.template(this.model.attributes));
         return this;
